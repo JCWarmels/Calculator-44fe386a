7bda1e859de6458f9b269ad07a40ec109c1c24a3
JCWarmels <jesse.warmels@student.rocva.nl>
2020-01-14 13:36:59 +0100

    Uitwerking van de exercise

---
+++ calculator.php
@@ -0,0 +1,14 @@
+<?php
+echo('Welke Operator wil je toepassen? (+,-)' . PHP_EOL);
+$opr = readline('');
+echo('Wat is je eerste getal?' . PHP_EOL);
+$num1 = readline('');
+echo('Wat is je tweede getal?' . PHP_EOL);
+$num2 = readline('');
+if($opr == '+'){
+    echo('Uw resultaat is: ' . $num1 + $num2);
+}
+else if($opr == '-'){
+    echo('Uw resultaat is: ' . $num1 - $num2);
+}
+
