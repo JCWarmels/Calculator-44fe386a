7971232cb71bb2ede6e1831dab31c3b90f29f1ab
JCWarmels <jesse.warmels@student.rocva.nl>
2020-01-14 13:47:58 +0100

    Uitwerking van de exercise

--- calculator.php
+++ calculator.php
@@ -6,9 +6,9 @@ $num1 = readline('');
 echo('Wat is je tweede getal?' . PHP_EOL);
 $num2 = readline('');
 if($opr == '+'){
-    echo('Uw resultaat is: ' . $num1 + $num2);
+    echo('Uw resultaat is: ' . ($num1 + $num2));
 }
 else if($opr == '-'){
-    echo('Uw resultaat is: ' . $num1 - $num2);
+    echo('Uw resultaat is: ' . ($num1 - $num2));
 }
 
